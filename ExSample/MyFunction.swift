6d5239bad82089771d120d13fe5231d9c71511b5
jake.k <jake.k@hpcnt.com>
2022-06-09 12:48:48 +0900

    Implement

---
+++ ExSample/MyFunction.swift
@@ -0,0 +1,18 @@
+//
+//  MyFunction.swift
+//  ExSample
+//
+//  Created by Jake.K on 2022/06/09.
+//
+
+import Foundation
+
+public func a1() {
+  print("a1!")
+}
+func a2() {
+  print("a2!")
+}
+private func a3() {
+  print("a3!")
+}
